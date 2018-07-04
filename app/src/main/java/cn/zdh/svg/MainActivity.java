19d39eed8d4c78fb5f2134e17c5ad5fde0e53e46
zhudaihao <zhudaihao@wswtz.com>
2018-07-04 15:40:21 +0800

    Initial commit

---
+++ app/src/main/java/cn/zdh/svg/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.zdh.svg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.selector1);
+    }
+}
